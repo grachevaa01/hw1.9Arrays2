ed04e927afedabcf052194477f70beff215200c4
grachevaa01 <grachevaa01@mail.ru>
2023-01-28 20:14:41 +0300

    ДЗ не получилось выполнить. Извините

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-
+    }//
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
-}
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        for (int i =0; i<arr.length; i++){
+            System.out.println(arr [i]);
+        }
+        System.out.println();
+         int expensesPerMonth = (arr []);
+
+
+
+
+
+
+    }}
